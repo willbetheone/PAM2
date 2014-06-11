43faf2b7a76579ac8e7751a652c086b945dcc0f9
Rinat Abdrashitov <rindopuz23@gmail.com>
2014-06-11 15:41:33 -0400

    Added initial project files

---
+++ PAM2/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PAM2
+//
+//  Created by Rinat Abdrashitov on 2014-06-11.
+//  Copyright (c) 2014 Rinat Abdrashitov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PAM2Tests/PAM2Tests.m
@@ -0,0 +1,34 @@
+//
+//  PAM2Tests.m
+//  PAM2Tests
+//
+//  Created by Rinat Abdrashitov on 2014-06-11.
+//  Copyright (c) 2014 Rinat Abdrashitov. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface PAM2Tests : XCTestCase
+
+@end
+
+@implementation PAM2Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
