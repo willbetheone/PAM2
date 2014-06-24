bc87f3587e92985e4d0de7849da6baddd1574e29
Rinat Abdrashitov <rindopuz23@gmail.com>
2014-06-24 00:34:43 -0400

    transpose get_Mat3x3f matrix

--- PAM2/Source/RALogging/RALogManager.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  RALogManager.cpp
-//  PAM2
-//
-//  Created by Rinat Abdrashitov on 2014-06-17.
-//  Copyright (c) 2014 Rinat Abdrashitov. All rights reserved.
-//
-
-#include "RALogManager.h"
